fcd72c7ad1f1490c9c42504a1e9df55788bcb604
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-08-03 02:41:19 +0700

    Update tic-tac-toe-bot.py

--- tic-tac-toe-bot.py
+++ tic-tac-toe-bot.py
@@ -62,18 +62,18 @@ def move_check(call):
     player_id = call.from_user.id
     current_player_id = gm.player.id
     if player_id != current_player_id:
-        bot.answer_callback_query(call.id, "Wait for your turn")
+        bot.answer_callback_query(call.id, "Chờ đợi đến lượt của bạn")
     else:
         try:
             move = int(move)
         except ValueError:
-            bot.answer_callback_query(call.id, "Cell occupied")
+            bot.answer_callback_query(call.id, "Ô đã bị chiếm")
         else:
             gm.move(move)
             win_check(call.message)
 
 
-@bot.callback_query_handler(func=lambda call: True and call.data.startswith('players'))
+@bot.callback_query_handler(func=lambda call: True and call.data.startswith('người chơi'))
 def get_players(call):
     player_id = call.from_user.id
     player_name = call.from_user.first_name
@@ -83,13 +83,13 @@ def get_players(call):
         if not gm.playerX.id:
             gm.playerX.name = player_name
             gm.playerX.id = player_id
-            bot.send_message(call.message.chat.id, f"Player 1: {player_name} ({gm.playerX.sym})")
+            bot.send_message(call.message.chat.id, f"Người chơi 1: {player_name} ({gm.playerX.sym})")
         elif not gm.playerO.id:
             gm.playerO.name = player_name
             gm.playerO.id = player_id
-            bot.send_message(call.message.chat.id, f"Player 2: {player_name} ({gm.playerO.sym})")
+            bot.send_message(call.message.chat.id, f"Người chơi 2: {player_name} ({gm.playerO.sym})")
     else:
-        bot.answer_callback_query(call.id, "Wait for player 2")
+        bot.answer_callback_query(call.id, "Chờ người chơi 2")
 
     if gm.playerO.id and gm.playerX.id:
         gm.reset()
@@ -98,7 +98,7 @@ def get_players(call):
 
 @bot.message_handler(commands=['start'], chat_types=['group'])
 def begin_mp(message):
-    bot.send_message(message.chat.id, "Tic-tac-toe: Multiplayer")
+    bot.send_message(message.chat.id, "Caro: nhiều người chơi")
     gm = logic.Game()
     s.set_gm(message.chat.id, gm)
     gm.full_reset()
@@ -106,12 +106,12 @@ def begin_mp(message):
     gm.playerO.id = None
     gm.playerX.id = None
     keyboard = Keyboa(items=InlineKeyboardButton('Play', callback_data='players'), items_in_row=2)
-    bot.send_message(chat_id=message.chat.id, text="Waiting for players...", reply_markup=keyboard())
+    bot.send_message(chat_id=message.chat.id, text="Chờ người chơi...", reply_markup=keyboard())
 
 
 @bot.message_handler(commands=['start'], chat_types=['private'])
 def begin_sp(message):
-    bot.send_message(message.chat.id, "Tic-tac-toe: Single player")
+    bot.send_message(message.chat.id, "Caro: 1 người chơi")
     gm = logic.Game()
     s.set_gm(message.chat.id, gm)
     gm.reset()
@@ -125,10 +125,10 @@ def begin_sp(message):
 @bot.message_handler(commands=['help'], chat_types=['group', 'private'])
 def help_message(message):
     text = """
-    Hello! 
-    This is a tic-tac-toe bot. You can play in single mode against AI
-    or in multiplayer mode in groups (add the bot to your group to play).
-    Bot username: @tic_tac_t0e_bot
+   Xin chào!
+    Đây là một bot Caro. Bạn có thể chơi ở chế độ đơn chống lại AI
+    hoặc ở chế độ nhiều người chơi theo nhóm (thêm bot vào nhóm của bạn để chơi).
+    Tên người dùng bot: @tic_tac_t0e_bot
     New game: /start
     """
     bot.send_message(message.chat.id, dedent(text))
@@ -136,7 +136,7 @@ def help_message(message):
 
 @bot.message_handler(commands=['cancel'], chat_types=['group', 'private'])
 def begin(message):
-    bot.send_message(message.chat.id, 'Game cancelled')
+    bot.send_message(message.chat.id, 'Trò chơi bị hủy')
     gm = s.get_gm(message.chat.id)
     gm.full_reset()
 
@@ -152,7 +152,7 @@ def start(message):
     elif gm.player.name == 'Draw':
         win_check(message)
     else:
-        text = f"Player: {gm.player.name} ( {gm.player.sym} )\nPress [1-9] to play"
+        text = f"Player: {gm.player.name} ( {gm.player.sym} )\nNhấn [1-9] để chơi"
         keyboard = board(gm)
         message_id = bot.send_message(chat_id=message.chat.id, text=text, reply_markup=keyboard()).message_id
         s.set_last_message(message.chat.id, message_id)
@@ -163,7 +163,7 @@ def win_check(message):
     winner = gm.win_check()
     if winner:
         keyboard = board(gm)
-        bot.send_message(chat_id=message.chat.id, text=f"Winner: {winner.name}", reply_markup=keyboard())
+        bot.send_message(chat_id=message.chat.id, text=f"Người thắng: {winner.name}", reply_markup=keyboard())
         text = ""
         for i in gm.player_list:
             text += f"{i.name}: {i.total_score}\n"
@@ -172,7 +172,7 @@ def win_check(message):
         buttons = [InlineKeyboardButton('Yes', callback_data='ng yes'),
                    InlineKeyboardButton('No', callback_data='ng no')]
         keyboard = Keyboa(items=buttons, items_in_row=2)
-        message_id = bot.send_message(chat_id=message.chat.id, text="Play again?", reply_markup=keyboard()).message_id
+        message_id = bot.send_message(chat_id=message.chat.id, text="Chơi lại?", reply_markup=keyboard()).message_id
         s.set_last_message(message.chat.id, message_id)
     else:
         start(message)
